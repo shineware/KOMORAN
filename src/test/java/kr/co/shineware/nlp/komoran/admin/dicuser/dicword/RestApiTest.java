c3e27542534ba813fe2461c113df63e1c6d0070c
Junghwan Park <reserve.dev@gmail.com>
2019-04-01 22:49:54 +0900

    DicUser 테스트 추가

---
+++ src/test/java/kr/co/shineware/nlp/komoran/admin/dicuser/dicword/RestApiTest.java
@@ -0,0 +1,17 @@
+package kr.co.shineware.nlp.komoran.admin.dicuser.dicword;
+
+import kr.co.shineware.nlp.komoran.admin.AbstractRestIntegrationTest;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RestApiTest extends AbstractRestIntegrationTest {
+    @Before
+    public void init() {
+
+    }
+
+    @Test
+    public void NothingYet_Test() {
+        // Nothing Yet
+    }
+}
