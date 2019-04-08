18b77778c1a6855f78e1aa4682e005743ad611c8
Junsoo Shin <jsshin@shineware.co.kr>
2019-04-08 23:04:45 +0900

    KOMORAN-56 grammar.in 내부 규칙 정리 요청 - 모델 트레이닝 예제 추가 - 트레이닝 시 품사를 모두 대문자로 변환 - <start>, <end>를 BOE, EOE로 변환

---
+++ _rst/api/kr/co/shineware/nlp/komoran/corpus/builder/package-index.rst
@@ -0,0 +1,10 @@
+kr.co.shineware.nlp.komoran.corpus.builder
+==========================================
+
+.. java:package:: kr.co.shineware.nlp.komoran.corpus.builder
+
+.. toctree::
+   :maxdepth: 1
+
+   CorpusBuilder
+
---
+++ src/test/java/kr/co/shineware/nlp/komoran/core/Training.java
@@ -0,0 +1,29 @@
+package kr.co.shineware.nlp.komoran.core;
+
+import kr.co.shineware.nlp.komoran.corpus.builder.CorpusBuilder;
+import kr.co.shineware.nlp.komoran.modeler.builder.ModelBuilder;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+
+public class Training {
+    @Test
+    @Ignore
+    public void training() {
+        CorpusBuilder corpusBuilder = new CorpusBuilder();
+        corpusBuilder.buildPath("tagged_corpus", "tag");
+        corpusBuilder.setExclusiveIrrRule("resources/irrDic.remove.txt");
+        corpusBuilder.save("corpus_build");
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.setExternalDic("user_data" + File.separator + "wiki.titles");
+        modelBuilder.buildPath("corpus_build");
+        modelBuilder.save("models_full");
+
+        modelBuilder = new ModelBuilder();
+        modelBuilder.buildPath("corpus_build");
+        modelBuilder.save("models_light");
+
+    }
+}
