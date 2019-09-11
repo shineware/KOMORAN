13a970edee7843599543dcfc3659f1201003e007
Junghwan Park <reserve.dev@gmail.com>
2019-09-11 22:23:59 +0900

    모델 관리 메뉴 추가 (모델 삭제 및 배포 기능) closes #15 #24

--- src/main/java/kr/co/shineware/nlp/komoran/admin/controller/UserModelController.java
+++ src/main/java/kr/co/shineware/nlp/komoran/admin/controller/UserModelController.java
@@ -1,15 +1,20 @@
 package kr.co.shineware.nlp.komoran.admin.controller;
 
+import kr.co.shineware.nlp.komoran.admin.exception.ParameterInvalidException;
+import kr.co.shineware.nlp.komoran.admin.exception.ServerErrorException;
 import kr.co.shineware.nlp.komoran.admin.service.UserModelService;
 import kr.co.shineware.nlp.komoran.admin.util.ResponseDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 @RestController
@@ -26,7 +31,6 @@ public class UserModelController {
         ResponseDetail responseDetail = new ResponseDetail();
 
         List<String> modelList = userModelService.getModelList();
-        Collections.sort(modelList, Collections.reverseOrder());
 
         responseDetail.setData(modelList);
 
@@ -35,9 +39,57 @@ public class UserModelController {
 
 
     @GetMapping(value = "/new")
-    public ResponseDetail makeNewModel() {
+    public ResponseDetail makeNewUserModel() {
         userModelService.buildNewModel();
 
         return getModelList();
     }
+
+
+    @PostMapping(value = "/delete")
+    public ResponseDetail deleteUserModel(@RequestParam("modelName") String modelNameInRaw) {
+        if (modelNameInRaw == null || "".equals(modelNameInRaw)) {
+            throw new ParameterInvalidException("잘못된 모델명 [" + modelNameInRaw + "]");
+        }
+
+        ResponseDetail responseDetail = new ResponseDetail();
+
+        boolean isDeletedClearly = userModelService.deleteUserModel(modelNameInRaw);
+
+        if (!isDeletedClearly) {
+            throw new ServerErrorException("모델 삭제 중 오류가 발생하였습니다.");
+        }
+
+        List<String> modelList = userModelService.getModelList();
+        responseDetail.setData(modelList);
+
+        return responseDetail;
+    }
+
+
+    @GetMapping(value = "/download/{modelName}")
+    public ResponseEntity<?> downloadFile(@PathVariable("modelName") String modelNameInRaw) {
+        if (modelNameInRaw == null || "".equals(modelNameInRaw)) {
+            throw new ParameterInvalidException("잘못된 모델명 [" + modelNameInRaw + "]");
+        }
+
+        File zipFileToDeploy = userModelService.deployUserModel(modelNameInRaw);
+        long zipFileSize = zipFileToDeploy.length();
+        ByteArrayResource resource;
+
+        try {
+            resource = new ByteArrayResource(Files.readAllBytes(zipFileToDeploy.toPath()));
+            zipFileToDeploy.delete();
+        } catch (IOException e) {
+            throw new ServerErrorException("");
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(zipFileSize)
+                .header("Content-Disposition", "attachment; filename=" + zipFileToDeploy.getName())
+                .header("Content-Transfer-Encoding", "binary")
+                .body(resource);
+    }
+
 }
