6589344abf9d54ea246cccf6143c6dacb9680877
Junsoo Shin <jsshin@shineware.co.kr>
2020-02-25 01:06:48 +0900

    optimize import

--- src/test/java/kr/co/shineware/nlp/komoran/benchmark/PerformanceValidation.java
+++ src/test/java/kr/co/shineware/nlp/komoran/benchmark/PerformanceValidation.java
@@ -1,6 +1,5 @@
 package kr.co.shineware.nlp.komoran.benchmark;
 
-import kr.co.shineware.ds.aho_corasick.AhoCorasickDictionary;
 import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
 import kr.co.shineware.nlp.komoran.core.Komoran;
 import kr.co.shineware.nlp.komoran.corpus.model.Dictionary;
@@ -8,7 +7,7 @@ import kr.co.shineware.util.common.file.FileUtil;
 import org.junit.Test;
 
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.List;
 
 public class PerformanceValidation {
 
@@ -47,7 +46,5 @@ public class PerformanceValidation {
         AhoCorasickSpeedTest ahoCorasickSpeedTest = new AhoCorasickSpeedTest();
         ahoCorasickSpeedTest.load(new Dictionary("corpus_build/dic.word"));
         System.out.println(ahoCorasickSpeedTest.doTestAndGetAverageElapsedTime(FileUtil.load2List("stress.test", StandardCharsets.UTF_8)));
-
-
     }
 }
