937d9e5778724bacfe02a1b7ccfffe8a3545d258
신준수 <shin285@naver.com>
2026-03-09 23:46:01 +0900

    admin, elasticsearch-plugin 모듈 제거 및 Gradle 9 / JVM 17 업그레이드
    - admin, elasticsearch-plugin 모듈 삭제
    - settings.gradle에서 제거된 모듈 참조 삭제
    - Gradle 8.12 → 9.0 업그레이드
    - JVM 타겟 17로 설정 (sourceCompatibility, targetCompatibility)

--- admin/src/main/java/kr/co/shineware/nlp/komoran/admin/util/parser/stream/StreamParser.java
+++
@@ -1,9 +0,0 @@
-package kr.co.shineware.nlp.komoran.admin.util.parser.stream;
-
-import java.util.stream.Stream;
-
-public interface StreamParser {
-    static Stream<?> parse(String input) {
-        return null;
-    }
-}
